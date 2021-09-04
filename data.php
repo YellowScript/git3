7032ec1967e62bd892c756839a19b9277466493b
Yellow_JS <89409984+YellowScript@users.noreply.github.com>
2021-09-04 23:53:50 +0700

    mengisi data binatang

--- data.php
+++ data.php
@@ -1,1 +1,14 @@
-
+binatang = {
+  burung:{
+    kaki:2,
+    berkembang biak:"bertelur",
+    warna: "putih"},
+  ayam:{
+    kaki:2,
+    berkembang biak:"bertelur",
+    warna: "cokelat"},
+ kambing:{
+    kaki:4,
+    berkembang biak:"melahirkan",
+    warna: "hitam"},
+}
